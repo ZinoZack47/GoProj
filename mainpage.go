d770cb0cb0f2ac4075cfd6de1b811c6e1b9f1e93
= <martinkalvin6@gmail.com>
2021-07-11 15:09:19 +0100

    Added a 404 Page

--- mainpage.go
+++ mainpage.go
@@ -11,6 +11,10 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Welcome to the Site</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "Contact us <a href=\"mailto:martinkalvin6@gmail.com\">our email</a> ")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, `<h1>ERROR 404: The page you are trying to reach does not exist.</h1>
+		<p>if you keep getting this page please contact us at out email.</p>`)
 	}
 }
 
