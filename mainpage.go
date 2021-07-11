8a54168d7375eef910b50698c9dd2504f07d67d8
= <martinkalvin6@gmail.com>
2021-07-11 14:56:47 +0100

    Added a route to a Contact page

--- mainpage.go
+++ mainpage.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to the Site</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to the Site</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "Contact us <a href=\"mailto:martinkalvin6@gmail.com\">our email</a> ")
+	}
 }
 
 func main() {
