1c1df68a3e394e2031b7fbad47fcdcea713fa6cc
= <martinkalvin6@gmail.com>
2021-07-11 17:09:12 +0100

    Added the handler to 404

--- mainpage.go
+++ mainpage.go
@@ -17,12 +17,23 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Contact us <a href=\"mailto:martinkalvin6@gmail.com\">our email</a>")
 }
 
-/*fmt.Fprint(w, `<h1>ERROR 404: The page you are trying to reach does not exist.</h1>
-<p>if you keep getting this page please contact us at out email.</p>`)*/
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h6>Is fallout 76 good?</h6>")
+	fmt.Fprintf(w, "<p>Yes it is banned for racism. Thread Locked!</p>")
+}
+
+func error404notfound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, `<h1>ERROR 404: The page you are trying to reach does not exist.</h1>
+	<p>if you keep getting this page please contact us at out email.</p>`)
+}
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(error404notfound)
 	http.ListenAndServe(":3000", r)
 }
